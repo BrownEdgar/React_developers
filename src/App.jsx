73ad322b384c71b48c6e47eede5d3cd91348b75b
BrownEdgar <nikogosjanedgar@gmail.com>
2023-04-21 10:37:47 +0400

    first commit in my vite project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import db from './data/db.json'
+import './App.css'
+import Posts from './components/Posts';
+
+function App() {
+	const [data, setData] = useState(db.posts);
+	const handleDelete = (id) => { 
+		const filteredData = data.filter(post => post.id !== id);
+		setData(filteredData)
+	}
+	return (
+		<div>
+				<h1>Lesson 4</h1>
+			<Posts posts={data} handleDelete={handleDelete}/>
+		</div>
+	)
+}
+
+export default App
