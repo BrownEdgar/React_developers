aa3284e8a729f96b068229e1d98092ff188172c7
Ani Tamazyan <anitamazyan2000@gmail.com>
2023-05-16 22:40:57 +0400

    SorteTodos

---
+++ src/components/Ani/SortedTodos/Component.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './Component.css'
+
+
+export default function Component(props) {
+	return (
+        <div>
+            <div className='sort'>
+                <button onClick={() => props.sorteTrue()}>True First</button>
+                <button onClick={() => props.sorteFalse()}>False First</button>
+            </div>
+            
+            <div className='flex'>
+		 {
+			props.data.map(elem => {
+				return <div key={elem.id}>
+                    <div  onClick={() => props.todoToggle(elem.id)} >
+					    <h2>{elem.title}</h2>
+					    <span className={`${elem.completed ? "completed" : ""}`}></span>
+				    </div>
+                    <button onClick={() => props.deleteTodo(elem.id)}>X</button>
+                </div>
+                
+                
+			} )
+		 }
+		</div>
+        </div>
+	)
+}
+
+Component.propTypes = {
+	data: PropTypes.arrayOf(
+		PropTypes.shape({
+			userId: PropTypes.number.isRequired,
+			id: PropTypes.number.isRequired,
+			title: PropTypes.string.isRequired,
+			completed: PropTypes.bool.isRequired
+		})
+	),
+	todoToggle: PropTypes.func.isRequired,
+	sorteFalse: PropTypes.func.isRequired,
+	sorteTrue: PropTypes.func.isRequired,
+    deleteTodo: PropTypes.func.isRequired
+}
