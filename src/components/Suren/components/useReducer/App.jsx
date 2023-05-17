c265a92f24bc7e089b090481c5150d9a3be73235
Suren Kocharyan <115921644+skocharyann@users.noreply.github.com>
2023-05-17 22:45:27 +0400

    Added useReducer Homework

---
+++ src/components/Suren/components/useReducer/App.jsx
@@ -0,0 +1,81 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import reducer, { initialState } from './reducer';
+import './App.scss'
+
+export default function App () {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const getPosts = async () => {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+      dispatch({ type: 'FETCH_POSTS', payload: response.data });
+    } catch (err) {
+      console.error('Error fetching posts:', err);
+    }
+  };
+
+  const addDeveloper = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+
+    dispatch({ type: 'ADD_DEVELOPER', payload: name });
+
+    e.target.name.value = '';
+  };
+
+  const sortArray = () => {
+    dispatch({ type: 'SORT_ARRAY' });
+  };
+
+  const deletePost = (index) => {
+    dispatch({ type: 'DELETE_POST', payload: index });
+  };
+
+  return (
+    <div className='App'>
+      <h1>Actions: {state.actions}</h1>
+
+      <button onClick={getPosts}>Fetch Posts</button>
+
+      <form onSubmit={addDeveloper}>
+        <input type="text" name="name" placeholder="Developer Name" />
+        <button type="submit">Add Developer</button>
+      </form>
+
+      <button onClick={sortArray}>Sort Array</button>
+
+      <div>
+        <h2>Developers</h2>
+        <ul>
+          {state.developers.map((developer, index) => (
+            <li key={index}>{developer}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h2> Array to be Sorted</h2>
+        <ul>
+          {state.arr.map((number, index) => (
+            <li key={index}>{number}</li>
+          ))}
+        </ul>
+      </div>
+
+      <ul>
+        {state.posts.map((post, index) => (
+          <li key={post.id}>
+            {post.title}
+            <button onClick={() => deletePost(index)}>X</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+
+
+
+
