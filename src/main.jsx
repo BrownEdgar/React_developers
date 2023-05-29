15217af07022436b5dd0a0ed0cb96c0bf3a69d46
BrownEdgar <nikogosjanedgar@gmail.com>
2023-05-29 09:21:44 +0400

    fix code style

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/Edgar/moment/App'
+import App from './components/Ani/classNames/App'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 
