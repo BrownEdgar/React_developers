e6adc9f46d00a2ca303d0e334b6a751c2b9d4346
Suren Kocharyan <115921644+skocharyann@users.noreply.github.com>
2023-05-31 20:29:19 +0400

    added some changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/Component'
-import { BrowserRouter as Router } from 'react-router-dom'
+// import { BrowserRouter as Router } from 'react-router-dom'
+// import { Provider } from 'react-redux';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<Router>
-    <App />
-	</Router>
+
+<App />
+
+	
 )
