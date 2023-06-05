a01b021410ce70722612f8544c4f56c4691db56d
BrownEdgar <nikogosjanedgar@gmail.com>
2023-06-05 08:52:26 +0400

    added new wersion of reducers

--- src/store/features/usersSlice.js
+++ src/store/features/usersSlice.js
@@ -8,17 +8,20 @@ const initialUeersValue = {
 export default function usersReducer(state = initialUeersValue, action) {
 	switch (action.type) {
 
-		case DELETE_BY_ID: return {...state, users: state.users.filter(user => user.id !== action.payload.id)};
+		case DELETE_BY_ID: return {
+			...state, 
+			users: state.users.filter(user => user.id !== action.payload.id)
+		};
 
-		case DELETE_BY_NAME: return {...state, users: state.users.filter(user => user.username !== action.payload.username)};
+		case DELETE_BY_NAME: return {
+			...state, 
+			users: state.users.filter(user => user.username !== action.payload.username)};
 
 		case ADD_USER: return {...state, users: [...state.users, action.payload.user]}
 
-		case USER_BY_LOGIN: return {...state, result: state.users.find(user => user.login === action.payload.login)};
+		case USER_BY_LOGIN: return {...state, result: state.users.find(user => user.login === action.payload.login) || 'not found'};
 
-		case DELETE_BY_ID: return {...state, users: state.users.filter(user => user.id !== action.payload.id)};
-
-		case SORT_BY_NAME: return {...state, result: state.users.toSorted((a, b) => a.username - b.username)};
+		case SORT_BY_NAME: return {...state, result: state.users.toSorted((a, b) => a.username > b.username)};
 		
 		default: return state;
 	}
