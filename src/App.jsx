cd45c0ed435a764ca6b34676a3e1dfaa92e6b08f
BrownEdgar <nikogosjanedgar@gmail.com>
2023-06-16 10:28:56 +0400

    fix code style

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,19 @@ import './App.css'
 // import { addTodo, allDone,  } from "./features/todo/todoSlice";
 
 
-export default function App () {
-    // const todos = useSelector((state) => state.todo)
-    const dispatch = useDispatch()
-///////////////  todo functions /////////////////////////////
-    // const setAllDone = () => {
-    //     dispatch(allDone())
-    // }
+export default function App() {
+	// const todos = useSelector((state) => state.todo)
+	const dispatch = useDispatch()
+	///////////////  todo functions /////////////////////////////
+	// const setAllDone = () => {
+	//     dispatch(allDone())
+	// }
 
-    // const handleRemById = (id) => {
-    //     dispatch(removeById(id))
-    // }
+	// const handleRemById = (id) => {
+	//     dispatch(removeById(id))
+	// }
 
-    // const handleAdd = () => {
+	// const handleAdd = () => {
 	// 		const newTodo = {
 	// 			"userId": 1,
 	// 			"id": Date.now(),
@@ -27,37 +27,35 @@ export default function App () {
 	// 			"completed": false
 	// 		}
 	// 		dispatch(addTodo(newTodo))
-    // }
+	// }
 
-    // const setToJson = () => {
-    //     return JSON.stringify(todos)
-    // }
+	// const setToJson = () => {
+	//     return JSON.stringify(todos)
+	// }
 
-    // const allTodos = () => {
-    //     return (
-    //         <ul>
-    //             {todos.map(todo => (
-    //                 <li key={todo.id}>{todo.title}</li>
-    //             ))}
-    //         </ul>
-    //     )
-    // }
+	// const allTodos = () => {
+	//     return (
+	//         <ul>
+	//             {todos.map(todo => (
+	//                 <li key={todo.id}>{todo.title}</li>
+	//             ))}
+	//         </ul>
+	//     )
+	// }
 
-    const users = useSelector(selectAllUsers)
-    const filteredUsers = useSelector(selectUsersByFavoriteColor)
-    const [currentButtonIndex, setCurrentButtonIndex] = useState(-1)
+	const users = useSelector(selectAllUsers)
+	const filteredUsers = useSelector(selectUsersByFavoriteColor)
+	const [currentButtonIndex, setCurrentButtonIndex] = useState(-1)
 
-    const setActiveButtonIndex = (indx, color) => {
-        setCurrentButtonIndex(indx)
-        dispatch(setFilter(color))
-    }
+	const setActiveButtonIndex = (indx, color) => {
+		setCurrentButtonIndex(indx)
+		dispatch(setFilter(color))
+	}
 
+	return (
+		<div className="App">
 
-
-    return(
-        <div className="App">
-            
-            {///////////// todo-nery nkarum enq ejum
+			{///////////// todo-nery nkarum enq ejum
             /* <button onClick={setAllDone}> Set all done </button>
             <button onClick={() => handleRemById(3)}> Remove todo by id </button>
             <button onClick={handleAdd}> Add new todo </button>
@@ -65,49 +63,47 @@ export default function App () {
             <div>{setToJson()}</div>
             {allTodos()} */}
 
-            <h1>our users</h1>
-                <div className="grid">
-                    
-                    <button 
-                        onClick={() => setActiveButtonIndex(-1, 'all')}
-                        className={classNames({
-                            active:currentButtonIndex < 0
-                        })}>all</button>
+			<h1>our users</h1>
+			<div className="grid">
 
 
-                    <div className="filters">
+				<div className="filters">
+					<button
+						onClick={() => setActiveButtonIndex(-1, 'all')}
+						className={classNames({
+							active: currentButtonIndex < 0
+						})}>all</button>
+					{
+						
+						Array.from(new Set(users.map(user => user.favoriteColor)))
+							.map((color, index) => {
+								return (
+									<button
+										onClick={() => setActiveButtonIndex(index, color)}
+										className={classNames({
+											active: currentButtonIndex === index
+										})}>{color}</button>
+								)
+							})
+					}
 
-                        {
-
-                            Array.from(new Set(users.map(user => user.favoriteColor)))
-                            .map(color => {
-                                return (
-                                    <button 
-                                    onClick={() => setActiveButtonIndex(index, color)} 
-                                    className={classNames({
-                                       active: currentButtonIndex === index                                       
-                                    })}>{color}</button>
-                                )
-                            })
-                        }
-
-                    </div>
-                    <div className="users">
-                        {filteredUsers.map(user => {
-                            return (
-                                <article 
-                                key={user.id} 
-                                style={{backgroundColor: `${user.favoriteColor}`}}
-                                className={classNames({
-                                    white: user.favoriteColor === "white",
-                                    "white-text": user.favoriteColor === "black"
-                                })}>
-                                    <h2>{user.name.charAt(0)}</h2>
-                                </article>
-                            )
-                        })}
-                    </div>
-                </div>
-        </div>
-    )
+				</div>
+				<div className="users">
+					{filteredUsers.map(user => {
+						return (
+							<article
+								key={user.id}
+								style={{ backgroundColor: `${user.favoriteColor}` }}
+								className={classNames({
+									white: user.favoriteColor === "white",
+									"white-text": user.favoriteColor === "black"
+								})}>
+								<h2>{user.name.charAt(0)}</h2>
+							</article>
+						)
+					})}
+				</div>
+			</div>
+		</div>
+	)
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import  todoSlice  from '../features/todo/todoSlice'
 import filterSlice from "../features/filter/filterSlice";
+import usersSlice from "../features/users/usersSlice";
 
 
 export const store = configureStore({
     reducer: {
         // todo: todoSlice,
-        filter: filterSlice
+        filter: filterSlice,
+				users:usersSlice
     }
 })
 
